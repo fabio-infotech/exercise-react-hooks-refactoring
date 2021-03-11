e61fafd3c5dfca13f3e3dbf951697bf6faa43b11
Fabio Ferreira <fabio35547@gmail.com>
2021-03-11 20:48:41 -0300

    exercise-two concluido

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,63 +1,35 @@
 // src/context/Provider.js
 
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import CarsContext from './CarsContext';
+import React, { createContext, useState } from 'react';
 
-class Provider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cars: {
-        red: false,
-        blue: false,
-        yellow: false,
-      },
-      signal: {
-        color: 'red',
-      },
-    }
-    this.moveCar = this.moveCar.bind(this);
-    this.changeSignal = this.changeSignal.bind(this);
-  }
+const Context = createContext();
 
-  moveCar(car, side) {
-    this.setState({
-      cars: {
-        ...this.state.cars,
-        [car]: side,
-      },
+const CarsProvider = ({ children }) => {
+  const [cars, setCars] = useState({
+    red: false,
+    blue: false,
+    yellow: false,
+  });
+  const [signal, setColor] = useState({ color: 'red' });
+
+  const moveCar = (car, side) => {
+    setCars({
+      ...cars,
+      [car]: side,
     });
   };
 
-  changeSignal(signalColor) {
-    this.setState({
-      signal: {
-        ...this.state.signal,
-        color: signalColor,
-      },
-    });
+  const changeSignal = (signalColor) => {
+    setColor({ color: signalColor });
   };
 
-  render() {
-    const context = {
-      ...this.state,
-      moveCar: this.moveCar,
-      changeSignal: this.changeSignal,
-    };
+  const context = { cars, moveCar, signal, changeSignal };
 
-    const { children } = this.props;
-
-    return (
-      <CarsContext.Provider value={context}>
-        {children}
-      </CarsContext.Provider>
-    );
-  }
+  return (
+    <Context.Provider value={context}>
+      {children}
+    </Context.Provider>
+  );
 };
 
-Provider.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-export default Provider;
+export { Context, CarsProvider as Provider };
